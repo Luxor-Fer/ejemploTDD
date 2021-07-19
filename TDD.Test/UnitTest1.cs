e0efcc8969018cc3b1600bb586447094aa13d77b
Luxor-Fer <lzerna1972@uta.edu.ec>
2021-07-18 23:02:13 -0500

    Agregar archivos de proyecto.

---
+++ TDD.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void RaizCuadrada_4()
+        {
+            //Arrange: Crear el entorno de Trabajo
+            RaizCuadrada service = new RaizCuadrada();
+
+            //Act: Operacion
+            double result = service.CalcularRaizCuadrada(4);
+
+            //Asset: Verificar la condicion o el criterio de aceptacion de la prueba
+            Assert.AreEqual(2, result);
+        }
+
+    }
+}
---
+++ TDD/RaizCuadrada.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD
+{
+    public class RaizCuadrada
+    {
+        public double CalcularRaizCuadrada(int num)
+        {
+            if (num == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return Math.Sqrt(num);
+            }
+        }
+
+    }
+}
